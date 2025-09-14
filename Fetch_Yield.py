9cbdfe304758fc45764be247d4f9e58c99965f5b
glorymary96 <glorym.96@gmail.com>
2025-09-14 21:30:02 +0200

    fetch yield

---
+++ Fetch_Yield.py
@@ -0,0 +1,50 @@
+import requests
+import pandas as pd
+import numpy as np
+from config import Config
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("Fetch_Yield").getOrCreate()
+
+# Replace with your own API key from https://quickstats.nass.usda.gov/api
+API_KEY = Config.USDA.PARAMS["key"]
+
+# Define API endpoint
+BASE_URL = "https://quickstats.nass.usda.gov/api/api_GET/"
+
+def get_yield_data(state, year_GE=2000):
+    params = {
+        "key": API_KEY,
+        "year__GE": 2000,
+        "state_name": state,
+        "agg_level_desc": "COUNTY",        # AGRICULTURAL DISTRICT
+        "commodity_desc": "CORN",          # <-- change crop here
+        "statisticcat_desc": "YIELD",
+        "unit_desc": "BU / ACRE",          # <-- crop-specific unit
+        "format": "JSON"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json().get("data", [])
+
+    return data
+
+if __name__ == "__main__":
+    sc = spark.sparkContext
+
+    years = np.arange(2000, 2024, 1)
+    States =["Illinois", "Indiana", "Iowa", "Minnesota",
+             "Missouri", "Nebraska", "North Dakota",
+              "Ohio", "South Dakota"]
+
+    rdd_data = sc.parallelize(States).flatMap(get_yield_data)
+
+    data = spark.createDataFrame(rdd_data)
+
+    data.printSchema()
+
+
+
+
+
